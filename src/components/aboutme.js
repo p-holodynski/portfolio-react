abb1ec06e5d5d8c3179ade7c90137e58af1570be
Patryk Holodynski <p.holodynski@gmail.com>
2019-04-11 20:43:24 +0100

    projects page finished

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
+import {Grid, Cell} from 'react-mdl';
 
 class About extends Component {
     render(){
         return(
-            <div>
-                <h1>About page</h1>
+            <div className="contact-body">
+                <Grid className="contact-grid">
+                    <Cell col={6}>half page</Cell>
+                    <Cell col={6}>half page</Cell>
+                </Grid>
             </div>
         )
     }
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,67 @@
 import React, {Component} from 'react';
+import {Tabs, Tab, Grid, Cell, Card, CardActions, CardTitle, Button, CardMenu, IconButton, CardText} from 'react-mdl';
 
 class Project extends Component {
+    constructor(props){
+        super(props);
+        this.state = {activeTab: 0};
+    }
+
+    toggleCategories(){
+        if(this.state.activeTab === 0){
+            return(
+            <div className="project-grid">
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height:'176px', background: 'url(http://blog.addthiscdn.com/wp-content/uploads/2014/11/addthis-react-flux-javascript-scaling.png) center / cover'}}>
+                        Quote Generator
+                    </CardTitle>
+                    <CardText>
+                        This is the FreeCodeCamp Quote Generator project using React
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>GitHub</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height:'176px', background: 'url(http://blog.addthiscdn.com/wp-content/uploads/2014/11/addthis-react-flux-javascript-scaling.png) center / cover'}}>
+                        Quote Generator
+                    </CardTitle>
+                    <CardText>
+                        This is the FreeCodeCamp Quote Generator project using React
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>GitHub</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+            </div>
+            )
+        } else if(this.state.activeTab === 1){
+            return(
+                <div><h1>This is Angular</h1></div>
+            )
+        }
+    }
+
     render(){
         return(
-            <div>
-                <h1>Project page</h1>
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({activeTab: tabId})} ripple>
+                    <Tab>React</Tab>
+                    <Tab>Angular</Tab>
+                </Tabs>
+
+                <Grid>
+                    <Cell col={12}>
+                        <div className="content">{this.toggleCategories()}</div>
+                    </Cell>
+                </Grid>
             </div>
         )
     }
