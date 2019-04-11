ab705fa18be81620c647c3b53901a6428d76cf8d
Patryk Holodynski <p.holodynski@gmail.com>
2019-04-11 16:21:04 +0100

    gradient implemented

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import { Link } from 'react-router-dom';
 class App extends Component {
   render() {
     return (
-      < div className = "demo-big-content" >
+      <div className = "demo-big-content" >
         <Layout>
-          <Header title="Title" scroll>
+          <Header className="header-color" title="Title" scroll>
             <Navigation>
               <Link to="/resume">Resume</Link>
               <Link to="/aboutme">About Me</Link>
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
+import {Grid, Cell} from 'react-mdl';
 
 class LandingPage extends Component {
     render(){
         return(
-            <div>
-                <h1>Landing page</h1>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img 
+                            src="https://cdn.r10.net/image.php?u=102744&dateline=1525381113"
+                            alt="coderAvatar"
+                            className="avatar-img"
+                        />
+                    <div className="banner-text">
+                        <h1>Junior Web Developer</h1>
+                    </div>
+                    </Cell>
+                </Grid>
             </div>
         )
     }
