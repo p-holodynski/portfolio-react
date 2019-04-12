73843a7d8f26fceafff4bca60a3f3e6b7ae79645
Patryk Holodynski <p.holodynski@gmail.com>
2019-04-12 17:43:19 +0100

    Last touches

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -11,8 +11,20 @@ class About extends Component {
                     <p>Hi, my name is Patryk.</p>
                     <p>I graduated from the Open University with a BSc degree (Computing & IT)</p>
                     <p>I am currently learning as much as I can to become a Junior Web Developer</p>
-                    <p>I have created this portfolio to showcase some of my work using React</p>
-                    <p>If you want to know more visit my GitHub or LinkedIn</p>
+                    <p>My main focus point right now is the React framework</p>
+                    <p>I have created this portfolio to showcase some of my work</p>
+                    <p>If you want to know more visit my GitHub
+                        <a href="https://github.com/p-holodynski" target="_blank" rel="noopener noreferrer">
+                        <i className="fab fa-github-square" aria-hidden="true" />
+                        </a>
+                        or LinkedIn
+                        <a href="https://www.linkedin.com/in/patryk-holodynski/" target="_blank" rel="noopener noreferrer">
+                        <i className="fab fa-linkedin" aria-hidden="true" />
+                        </a>
+                        or FreeCodeCamp
+                        <a href="https://www.freecodecamp.org/p-holodynski" target="_blank" rel="noopener noreferrer">
+                        <i className="fab fa-free-code-camp" aria-hidden="true" />
+                        </a></p>
                 </div>
             </div>
         )
