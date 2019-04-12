1615ba6bbb1821819c5ecfe90baa6627a1edad17
Patryk Holodynski <p.holodynski@gmail.com>
2019-04-12 11:01:12 +0100

    Contact page template

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,10 +1,38 @@
 import React, {Component} from 'react';
+import {Grid, Cell, List, ListItem, ListItemContent} from 'react-mdl';
 
 class Contact extends Component {
     render(){
         return(
-            <div>
-                <h1>Contact page</h1>
+            <div className="contact-body">
+                <Grid className="contact-grid">
+                    <Cell col={6}>
+                        <h2>Patryk Holodynski</h2>
+                        <img 
+                            src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+                            alt="avatar"
+                            style={{height: '250px'}}
+                        />
+                        <p style={{width: '75%', margin: 'auto', paddingTop: '1em'}}>I am a recent graduate from Open University. I am currently learning and working on different projects to become a Front-End Developer.</p>
+                    </Cell>
+                    <Cell col={6}>
+                        <h2>Contact me</h2>
+                        <hr />
+                        <div className="contact-list">
+                            <List>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton'}} icon="person">Bryan Cranston</ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent icon="person">Aaron Paul</ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent icon="person">Bob Odenkirk</ListItemContent>
+                                </ListItem>
+                            </List>
+                        </div>
+                    </Cell>
+                </Grid>
             </div>
         )
     }
